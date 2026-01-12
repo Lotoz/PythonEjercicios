11bed18836e48c625bdced60e996ed26b780c80e
Lotoz <144383551+Lotoz@users.noreply.github.com>
2026-01-12 21:13:27 +0100

    DjangoActividad5

--- Unidades/Django/PrimerProyecto/HolaMundo/admin.py
+++ Unidades/Django/PrimerProyecto/HolaMundo/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # Register your models here.
 #Añadimos esta linea para comenzar con el registro.
 from HolaMundo.models import Author
+from HolaMundo.models import Book
 #Forma de registrar la tabla Author
 admin.site.register(Author)
+#Registrar tabla libro
+admin.site.register(Book)
